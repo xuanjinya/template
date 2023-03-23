d42ae7e6462d682570ded0d16b2b6a44735dc7af
xuanjinya <huangbeijin@qq.com>
2023-03-23 22:38:20 +0800

    first commit

---
+++ src/main/java/com/example/template/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.example.template.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("Haha!!!");
+        return "Hello World!";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test Success";
+    }
+
+}
