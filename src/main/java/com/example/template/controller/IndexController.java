23e05de90ac745eac27399ced318354191192f86
xuanjinya <huangbeijin@qq.com>
2023-03-23 23:21:49 +0800

    1111

--- src/main/java/com/example/template/controller/IndexController.java
+++ src/main/java/com/example/template/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index() {                                                   
         return "Hello World!";
     }
 
